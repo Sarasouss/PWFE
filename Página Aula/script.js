5f0cac8bc1c7380cc61cd862fd53827906090be0
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-08-16 10:41:57 -0300

    16/08

---
+++ Página Aula/script.js
@@ -0,0 +1,5 @@
+console.log("Cabeçalho com imagem")
+console.log("Logo abaixo Titulo sobre o que a página fala")
+console.log("Tabela com imagens e texto  (em forma de botão) sobre o Top 10")
+console.log("Video de trailer do filme")
+console.log("Rodapé com logo da Marvel")
