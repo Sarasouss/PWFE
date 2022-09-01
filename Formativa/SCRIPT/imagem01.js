45229592ff88ba345de27d7421b60735ec572f34
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-09-01 11:34:40 -0300

    2.0

---
+++ Formativa/SCRIPT/imagem01.js
@@ -0,0 +1,13 @@
+var img1 = document.getElementById("playerCustomizado"); //Criando a id para colocar no html
+
+function ampliar() {
+    meuVideo.width = 650;
+}//Botão para ampliar
+
+function reduzir(){
+    meuVideo.width = 300;
+}//Botão para reduzir
+
+function tamanhoNormal(){
+     meuVideo.width = 410;
+}//Botão de tamanho normal
