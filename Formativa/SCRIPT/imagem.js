a2ea545b4da301348d4f9707f3aed6dfd527ced5
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-09-02 11:36:15 -0300

    Add files via upload

---
+++ Formativa/SCRIPT/imagem.js
@@ -0,0 +1,55 @@
+var img1 = document.getElementById("imagem1"); 
+
+function ampliar1() {
+    img1.width = 650;
+}
+
+function reduzir1(){
+    img1.width = 300;
+}
+
+function tamanhoNormal1(){
+     img1.width = 410;
+}
+
+var img2 = document.getElementById("imagem2"); 
+
+function ampliar2() {
+    img2.width = 650;
+}
+
+function reduzir2(){
+    img2.width = 300;
+}
+
+function tamanhoNormal2(){
+     img2.width = 410;
+}
+
+var img3 = document.getElementById("imagem3"); 
+
+function ampliar3() {
+    img3.width = 650;
+}
+
+function reduzir3(){
+    img3.width = 300;
+}
+
+function tamanhoNormal3(){
+     img3.width = 410;
+}
+
+var img4 = document.getElementById("imagem4"); 
+
+function ampliar4() {
+    img4.width = 650;
+}
+
+function reduzir4(){
+    img4.width = 300;
+}
+
+function tamanhoNormal4(){
+     img4.width = 410;
+}
